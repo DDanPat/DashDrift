53c79f1f15d4cb256b9d0d5a797e3b3141d40500
DDanPat <hyoung6855@gmail.com>
2025-09-11 15:36:38 +0900

    Initial commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody carRB;
+    [SerializeField] private Transform[] rayPoints; // 자동차 바퀴 위치
+    [SerializeField] private LayerMask groundLayer; // 지면 레이어
+
+    [Header("서스펜션 설정")]
+    [SerializeField] private float springStiffness; // 스프링 강성
+    [SerializeField] private float damperStiffness; // 댐퍼 강성
+    [SerializeField] private float restLength; // 스프링 정지 길이
+    [SerializeField] private float springTravel; // 스프링 이동 거리
+    [SerializeField] private float wheelRadius; // 바퀴 반지름
+
+
+    private void Start()
+    {
+        carRB = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Suspension();
+    }
+
+    private void Suspension()
+    {
+        foreach (Transform rayPoint in rayPoints)
+        {
+            RaycastHit hit;
+            float maxLength = restLength + springTravel;
+
+            if (Physics.Raycast(rayPoint.position, -rayPoint.up, out hit, maxLength + wheelRadius, groundLayer))
+            {
+                float currentSpringLength = hit.distance - wheelRadius;
+                float springCompression = restLength - currentSpringLength / springTravel;
+
+                float springVelocity = Vector3.Dot(carRB.GetPointVelocity(rayPoint.position), rayPoint.up);
+                float dampForce = damperStiffness * springVelocity;
+
+                float springForce = springCompression * springStiffness;
+
+                float netForce = springForce - dampForce;
+
+                carRB.AddForceAtPosition(netForce * rayPoint.up, rayPoint.position);
+
+                Debug.DrawLine(rayPoint.position, hit.point, Color.red);
+            }
+            else
+            {
+                Debug.DrawLine(rayPoint.position,
+                    rayPoint.position + (wheelRadius + maxLength) * -rayPoint.up, Color.green);
+            }
+        }
+    }
+}
